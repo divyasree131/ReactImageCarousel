fc58dbde65b22cdf6a92cfa13f8653acc764411d
divyasree131 <divyasree131851@gmail.com>
2024-05-25 22:28:03 +0530

    added carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import "./styles.css";
-import { slides } from "./data/ImageData.json";
+import slides from "./data/ImageData";
 import { Carousel } from "./components/Carousel";
 
 export default function App() {
-  console.log(slides);
   return (
     <div className="App">
       <Carousel slides={slides} />
---
+++ src/data/ImageData.js
@@ -0,0 +1,7 @@
+const slides = [
+  { src: "https://picsum.photos/id/237/200/300", alt: "Image 1" },
+  { src: "https://picsum.photos/seed/picsum/200/300", alt: "Image 2" },
+  { src: "https://picsum.photos/200/300", alt: "Image 3" },
+];
+
+export default slides;
